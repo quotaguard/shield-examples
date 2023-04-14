b4f7d04777dd6902dd4c672c2bfef2ca1b61b947
Tim Day <tim@quotaguard.com>
2023-04-14 09:45:46 -0500

    Fix node https with axios example

--- node/https/axios/https.js
+++ node/https/axios/https.js
@@ -1,20 +1,21 @@
 const axios = require('axios');
 const url = require('url');
+const HttpsProxyAgent = require('https-proxy-agent');
 
 const proxy = url.parse(process.env.QUOTAGUARDSHIELD_URL);
 
+const proxyAgent = new HttpsProxyAgent({
+  protocol: 'https:',
+  host: proxy.hostname,
+  port: proxy.port,
+  auth: `${proxy.auth.split(':')[0]}:${proxy.auth.split(':')[1]}`
+});
+
 const fetchIp = async () => {
     try {
-        const res = await axios.get('https://ip.quotaguard.com',{
-          proxy: {
-            host: proxy.hostname,
-            port: proxy.port,
-            auth: {
-              username: proxy.auth.split(':')[0],
-              password: proxy.auth.split(':')[1]
-            }
-          }
-        });
+        const axiosInstance = axios.create({ httpsAgent: proxyAgent });
+
+        const res = await axiosInstance.get('https://ip.quotaguard.com');
         console.log(res.data);
     } catch (err) {
         console.error(err);
