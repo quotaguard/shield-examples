f22bce85e25d13afeb5b1ba29b4bde57be49d491
Randall Haire <randall@quotaguard.com>
2025-01-27 11:02:24 -0600

    feat: add https/curb README.md

--- ruby/https/curb/app.rb
+++ ruby/https/curb/app.rb
@@ -2,7 +2,7 @@
 require 'curb'
 
 proxy_url = ENV.fetch("QUOTAGUARDSHIELD_URL") do
-  abort "Set QUOTAGUARDSHIELD_URL, e.g. http://user:pass@us-east-shield-02.quotaguard.com:9293"
+  abort "Please set QUOTAGUARDSHIELD_URL environment variable."
 end
 
 begin
@@ -16,12 +16,6 @@ begin
   curl.proxy_url      = "#{parsed.scheme}://#{parsed.host}:#{parsed.port}"
   curl.proxy_tunnel   = true  # Tells libcurl to use CONNECT for HTTPS
   curl.proxypwd       = "#{parsed.user}:#{parsed.password}" if parsed.user && parsed.password
-
-  # Optional: If you really need to connect to the proxy over HTTPS itself
-  # you can try setting:
-  # curl.proxy_ssl_verify_peer = false
-  # curl.proxy_ssl_verify_host = 0
-  # But that’s not recommended for production.
 
   curl.perform
   puts "Status: #{curl.response_code}"
